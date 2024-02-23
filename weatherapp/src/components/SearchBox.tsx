0ce5d0c3f2cf5ceb9f2ba39c52436d3a52f5ef52
claytoncritchfield <clayton.critchfield@gmail.com>
2024-02-23 12:07:49 -0500

    Created and styled navbar complete with React Icons

---
+++ weatherapp/src/components/SearchBox.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+
+type Props = {
+className?: string;
+value:string;
+onChange:React.FormEventHandler<HTMLFormElement> | undefined;
+onSubmit:React.ChangeEventHandler<HTMLInputElement> | undefined;
+}
+
+export default function SearchBox(props: Props) {
+    return (
+        <form onSubmit={props.onSubmit} className={cn("flex relative items-center justify-center h-10", props.className)}>
+            <input
+                value={props.value}
+                onChange={props.onChange} 
+                type="text" placeholder="Search location..."
+                className="px-4 py-2 w-[230px] border border-gray-300 rounded-1-md focus:outline-none focus:border-blue-500 h-full"
+            ></input>
+            <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full">
+                <IoSearch />
+            </button>
+        </form>
+    )
+}
